f59485e0b1a94bb336ffa0455ee4af7262a6ccc6
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-08-20 12:56:38 -0700

    added select plants query

--- index.js
+++ index.js
@@ -5,43 +5,42 @@ require("dotenv").config();
 
 let db
 function run() {
-
-  db.run("select * from plants", function (err, results) {
-    if (err != null) {
-      console.log(err)
-    }
-    return Promise.all(
-      results.map(result => {
-        let device = parseInt(result.device, 16)
-        let client = sensor.open(device)
-        return client.getTemperature()
-          .then((temp) => {
-            let tempF = Math.round((temp * 9 / 5) + 32)
-            console.log(tempF);
-            db.add_temp([tempF, result.id], function (err, res) {
-              console.log(err);
+  db.get_all_plants()
+    .then(function (results) {
+      return Promise.all(
+        results.map(result => {
+          console.log(result)
+          let device = parseInt(result.device, 16)
+          let client = sensor.open(device)
+          return client.getTemperature()
+            .then((temp) => {
+              let tempF = Math.round((temp * 9 / 5) + 32)
+              console.log(tempF);
+              return db.add_temp([tempF, result.id])
             })
-          })
-          .then(() => {
-            return client.getMoisture()
-          })
-          .then(console.log)
-      })
-    )
-      .then(() => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve()
-          }, 30000)
+            .then(() => {
+              return client.getMoisture()
+            })
+            .then((moisture) => {
+              console.log(moisture);
+              return db.add_moisture([moisture, result.id])
+            })
         })
-      })
-      .then(() => {
-        return run()
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  })
+      )
+        .then(() => {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve()
+            }, 30000)
+          })
+        })
+        .then(() => {
+          return run()
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
 }
 
 function main() {
