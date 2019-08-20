5e47c6d280f25fc645979f8084ccc8f35c3d9925
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-08-20 13:30:46 -0700

    removed logs

--- index.js
+++ index.js
@@ -9,20 +9,17 @@ function run() {
     .then(function (results) {
       return Promise.all(
         results.map(result => {
-          console.log(result)
           let device = parseInt(result.device, 16)
           let client = sensor.open(device)
           return client.getTemperature()
             .then((temp) => {
               let tempF = Math.round((temp * 9 / 5) + 32)
-              console.log(tempF);
               return db.add_temp([tempF, result.id])
             })
             .then(() => {
               return client.getMoisture()
             })
             .then((moisture) => {
-              console.log(moisture);
               return db.add_moisture([moisture, result.id])
             })
         })
