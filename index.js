06ec8e1f21aa06e47b9d114b942e52ed241a9d28
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-08-20 11:57:03 -0700

    added recursion for data to be saved every 30 sec

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const massive = require('massive')
+const Massive = require('massive')
 const sensor = require('i2c-seesaw-moisture-sensor')
 require("dotenv").config();
-const controller = require('./controller')
 
-const app = express();
 
-app.use(bodyParser.json());
+let db
+function run() {
 
-let client = sensor.open(0x36)
-
-client.getTemperature()
-  .then((temp) => {
-    let tempF = Math.round((temp * 9 / 5) + 32)
-    console.log(tempF);
+  db.run("select * from plants", function (err, results) {
+    if (err != null) {
+      console.log(err)
+    }
+    return Promise.all(
+      results.map(result => {
+        let device = parseInt(result.device, 16)
+        let client = sensor.open(device)
+        return client.getTemperature()
+          .then((temp) => {
+            let tempF = Math.round((temp * 9 / 5) + 32)
+            console.log(tempF);
+            db.add_temp([tempF, result.id], function (err, res) {
+              console.log(err);
+            })
+          })
+          .then(() => {
+            return client.getMoisture()
+          })
+          .then(console.log)
+      })
+    )
+      .then(() => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve()
+          }, 30000)
+        })
+      })
+      .then(() => {
+        return run()
+      })
+      .catch(err => {
+        console.log(err);
+      })
   })
-  .then(() => {
-    return client.getMoisture()
-  })
-  .then(console.log)
+}
 
+function main() {
+  Massive(process.env.CONNECTION_STRING)
+    .then((database) => {
+      db = database
+    })
+    .then(() => {
+      run()
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
 
-
-massive(process.env.CONNECTION_STRING)
-  .then(database => {
-    app.set("db", database);
-    console.log("database is kickin");
-  })
-  .catch(error => {
-    console.log("-------------- database issue", error);
-  });
-
-app.get('/api/plantmoisture', controller.addMoisture)
-app.get('/api/planttemp', controller.addTemp)
-
-const PORT = 4001;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+main()
